bbe6f02219acd74509bf8e10df9b58e4b4964a30
deny39-gif <denyvicente15@gmail.com>
2026-05-12 12:32:53 -0400

    xbn

---
+++ truExcrpt.py
@@ -0,0 +1,35 @@
+# while True:
+#     try:
+#         num=int(input("ingrese un numero: "))
+#         break
+#     except:
+#         print("solamente ingresra numero enteros, no ingresar palabras")
+
+op=0
+total=0
+while op!=4:
+    try:
+        print("1.- Pc $500.000")
+        print("2.-  LGTV 55 pulgadas $450.000")
+        print("3.- Micri")
+        print("4.- salir")
+        print("eliga que comprar")
+        op=int(input())
+    except ValueError as e:
+        print("error", e)
+        print("solo se ha acepentan numeros enteros")
+        match op:
+            case 1:
+                print("el total a pagar es ", 500000*1.19)
+                total+=500000*1.19
+            case 2:
+                print("el total a pagar es ", 450000*1.19)
+                total+=450000*1.19
+            case 3:
+                print("el total a pagar es ", 100000*1.19)
+                total+=100000*1.19
+            case 4:
+                print("salida")
+                print("el total a pagar es ", total)
+            case _:
+                print("opcion invalidad")
