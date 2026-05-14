481f9be18840e6c7ca2b0d7b74244be400dc718e
deny39-gif <denyvicente15@gmail.com>
2026-05-14 13:26:06 -0400

    02d

--- truExcrpt.py
+++ truExcrpt.py
@@ -34,15 +34,62 @@
 #             case _:
 #                 print("opcion invalidad")
 
-cantidad=int(input("cuantos pasajes desas vender?: "))
-total=0
-for i in range(cantidad):
-    while True:
-        try:
-            precio=input(f"ingresa el precio del pasaje {i+1}: ")
-            precio=float(precio)
-            total+=precio
-        except ValueError:
-            print("debes ingresar un valor numerico")
-        break
-print(f"el total de ingresor del pasaje es: {total} ") 
+# cantidad=int(input("cuantos pasajes desas vender?: "))
+# total=0
+# for i in range(cantidad):
+#     while True:
+#         try:
+#             precio=input(f"ingresa el precio del pasaje {i+1}: ")
+#             precio=float(precio)
+#             total+=precio
+#         except ValueError:
+#             print("debes ingresar un valor numerico")
+#         break
+# print(f"el total de ingresor del pasaje es: {total} ") 
+
+saldo=100000
+
+while True:
+    print("---CAJERO AUTOMATICO---")
+    print("1.- consultar saldo")
+    print("2.- retirar dinero")
+    print("3.- depositar dinero")
+    print("4.- salir")
+    print("---CAJERO AUTOMATICO---")
+    try:
+        op=int(input("selecione una opcion: "))
+        if op==1:
+            print(f"su saldo es de ${saldo}")
+        elif op==2:
+            try:
+                retiro=int(input("ingrese el monto que desea retirar: "))
+                if retiro<=0:
+                    print("el monto debe ser mayor que 0")
+                elif retiro%5000!=0:
+                    print("solo se permiten montos de $5000")
+                elif retiro>saldo:
+                    print("saldo insuficiente")
+                else:
+                    saldo-=retiro
+                    print(f"retiro aprovado, su saldo es: {saldo}")
+            except ValueError:
+                print("debe ingresar un valor numerico valido")
+        elif op==3:
+            try:
+                deposito=int(input("ingresar el monto a depositar: "))
+                if deposito<=0:
+                    print("el monto debe ser mayor a 0")
+                elif deposito%5000!=0:
+                    print("solo se permiten montos multiple de $5000")
+                else:
+                    saldo+=deposito
+                    print(f"deposito aprovado, su saldo es: ${saldo}")
+            except ValueError:
+                print("debe ingresar un valor numerico valido")
+        elif op==4:
+            print("que tenga un buen dia caballero o dame")
+            break
+        else:
+            print("opcion invalida, por favor vuelva a intentarlo")
+    except ValueError:
+        print("por favor vuelva intentarlo")
--- while.py
+++ while.py
@@ -9,3 +9,19 @@
 #     print(num, " X ", cont, " = ", num*cont)
 #     cont+=1
 
+# code=4545
+# while True:
+#     pasw=int(input("ingresar su codigo de 4 digitos: "))
+#     if pasw==code:
+#         print("codigo aceptado")
+#         break
+#     else:
+#         print("codigo incorrecto")
+
+while True:
+    sex=input("ingrese su sexo (F/M):").lower
+    if sex =="m":
+        print("no se puede acceder aqui")
+    else:
+        print("adelante")
+        break
