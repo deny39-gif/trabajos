a6f75958323d5eab882a36683df01857bbe094f2
deny39-gif <denyvicente15@gmail.com>
2026-06-16 13:30:29 -0400

    materia4

--- ejerciciosdediccionarios.py
+++ ejerciciosdediccionarios.py
@@ -56,60 +56,134 @@
 
 # Menupokemon()
 
-produc={
-    1:{"producto": "Uva", "precio": 2000},
-    2:{"producto": "palta", "precio": 4000},
-    3:{"producto": "pera", "precio": 1500}
+# produc={
+#     1:{"producto": "Uva", "precio": 2000},
+#     2:{"producto": "palta", "precio": 4000},
+#     3:{"producto": "pera", "precio": 1500}
+# }
+
+# def mostr():
+#     print("="*30)
+#     for P, d in produc.items():
+#         print(f"{P}.-{d}")
+
+# def camb():
+#     print("="*30)
+#     mostr()
+#     actualizar=int(input("Que producto desea actualizar: "))
+#     nameP=input("ingresa el producto: ")
+#     Precio=input("cuanto cuasta ese producro: ")
+#     produc[actualizar]={"nombre": nameP, "nivel": Precio}
+#     print("actualizacion con exito")
+
+# def elimi():
+#     print("="*30)
+#     mostr()
+#     borrarpro=int(input("cual pokemon desea eliminar: "))
+#     del produc[borrarpro]
+
+# def agre():
+#     print("="*30)
+#     pro=input("ingrese el producto al carrito: ")
+#     pre=input("cuanrp cuesta este prodcuto: ")
+#     produc[list(produc.keys())[-1]+1]={"producto": pro, "nivel": pre}
+
+# def comp():
+#     mostr()
+#     compra=int(input("que producto desea comnprar)"))
+#     for compra in produc:
+#         print(f"usted a comprado {pro}")
+
+# while True:
+#     try:
+#         print("1.-agregar porducto")
+#         print("2.-cambiar producto")
+#         print("3.-eliminar producto")
+#         print("4.-mostar productos")
+#         print("5.- comprar")
+#         print("6.- Salir")
+#         print("="*30)
+#         op=int(input("que quiere hacer: "))
+#         if op==1:
+#             agre()
+#         elif op==2:
+#             camb()
+#         elif op==3:
+#             elimi()
+#         elif op==4:
+#             mostr()
+#         elif op==5:
+
+parking={
+    1:[2000,3500,2000,2000,3500,2000,2000,3500,2000],
+    2:[],
+    3:[],
+    4:[]
 }
 
-def mostr():
-    print("="*30)
-    for P, d in produc.items():
-        print(f"{P}.-{d}")
+tarifa={
+    "ligero": 2000,
+    "mediano": 3000,
+    "pesado": 3500,
+}
 
-def camb():
-    print("="*30)
-    mostr()
-    actualizar=int(input("Que producto desea actualizar: "))
-    nameP=input("ingresa el producto: ")
-    Precio=input("cuanto cuasta ese producro: ")
-    produc[actualizar]={"nombre": nameP, "nivel": Precio}
-    print("actualizacion con exito")
+Lig=0
+pes=0
+med=0
 
-def elimi():
-    print("="*30)
-    mostr()
-    borrarpro=int(input("cual pokemon desea eliminar: "))
-    del produc[borrarpro]
+ganacias=0
 
-def agre():
-    print("="*30)
-    pro=input("ingrese el producto al carrito: ")
-    pre=input("cuanrp cuesta este prodcuto: ")
-    produc[list(produc.keys())[-1]+1]={"producto": pro, "nivel": pre}
+def ingre():
+    global ganacias, Lig, pes, med
 
-def comp():
-    mostr()
-    compra=int(input("que producto desea comnprar)"))
-    for compra in produc:
-        print(f"usted a comprado {pro}")
+    tipo=input("Tipo de vehiculo (ligero, mediano, pesado)").lower()
+    if tipo=="ligero":
+        Lig+=1
+    elif tipo=="mediano":
+        med+=1
+    elif tipo=="pesado":
+        pes+=1
+    elif tipo not in tarifa:
+        print("Tipo de vehiculo invalido")
+        return
+    for piso in parking:
+        if len(parking[piso])<10:
+            parking[piso].append(tarifa[tipo])
+            ganacias+=tarifa[tipo]
+            print(f"vehiculos estacionados en {piso}")
+            print(f"espacios ocupados:{len(parking[piso])}/10")
+            return
 
 while True:
     try:
-        print("1.-agregar porducto")
-        print("2.-cambiar producto")
-        print("3.-eliminar producto")
-        print("4.-mostar productos")
-        print("5.- comprar")
-        print("6.- Salir")
         print("="*30)
-        op=int(input("que quiere hacer: "))
+        print("1.- Ingresar vehiculos")
+        print("2.- Contar ganancias")
+        print("3.- Contar vehiculos")
+        print("4.- Mostrar parking")
+        print("5.- Salir")
+        print("="*30)
+        op=int(input("seleciona una opcion: "))
+
         if op==1:
-            agre()
+            ingre()
         elif op==2:
-            camb()
+            print("las ganancias son:", ganacias)
         elif op==3:
-            elimi()
+            total=0
+            for piso in parking:
+                total+=len(parking[piso])
+            print("vehiculos en total:", total)
+            print("ligeros:", Lig)
+            print("medianos:", med)
+            print("Pesados:", pes)
         elif op==4:
-            mostr()
+            for piso in parking:
+                print(f"piso {piso}:{parking[piso]}")
         elif op==5:
+            print("se termino")
+            break
+        else:
+            print("opcion invalida")
+    except ValueError:
+        print("solamente numeros por favor")
--- pokemon.py
+++ pokemon.py
@@ -1,2 +1,56 @@
 import random
 
+attack=random.randint(5,35)
+captu=random.randint(1,4)
+hp1=100
+hp2=500
+
+def captura():
+    if captu==1:
+        print("el pokemon lugia no capturado")
+    elif captu==2:
+        print("el pokemon lugia logro escapar de la ball")
+    elif captu==3:
+        print("el pokemon lugia acaba de romper la ball")
+    elif captu==4:
+        print("pokemon lugia a sido capturado")
+        break
+
+movilugia={
+    "movi1": "Aeroblast",
+    "movi2": "Recuperacion",
+    "movi3": "Paz mental",
+    "movi4": "Rayo hielo"
+}
+
+movichar={
+    "movi1": "Danza dragon",
+    "movi2": "Puño trueno",
+    "movi3": "Garra dragon",
+    "movi4": "Lanzallamas"
+}
+
+mochi={
+    "pocion": 64,
+    "superpocion": 32,
+    "restauratotal": 6,
+    "pokeball": 16,
+    "superball": 32,
+    "ultraball": 64,
+}
+
+def pokedex():
+    print("Lugia el pokemon legendario de los mares")
+    print("tipo: volador y psiquico")
+
+def menu():
+    print("="*30)
+    print(".-")
+    print(".-")
+    print(".-")
+
+print(f"un lugia salvaje con {hp2} acaba de aparecer")
+print("Charizard yo te elijo")
+print(f"Charizard: {hp1} de hp")
+
+
