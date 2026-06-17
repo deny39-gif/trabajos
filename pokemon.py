0216bbc7bff4276682100220c8ee10c350a0e81a
deny39-gif <denyvicente15@gmail.com>
2026-06-16 22:09:05 -0400

    poke2

--- pokemon.py
+++ pokemon.py
@@ -56,10 +56,10 @@ def menu():
         if op==1:
             print("movimientos de charizard:")
             for clave, movimientos in movichar.items():
-                print(movichar["movie1"])
-                print(movichar["movie2"])
-                print(movichar["movie3"])
-                print(movichar["movie4"])
+                print("1.-",movichar["movie1"])
+                print("2.-",movichar["movie2"])
+                print("3.-",movichar["movie3"])
+                print("4.-",movichar["movie4"])
                 att=int(input("que desea hacer??: "))
                 if att==1:
                     print("la defensa de charizard aumentado")
@@ -74,6 +74,16 @@ def menu():
                         hp2-=attack
                         turnos+=1
                 elif att==3:
+                    print("charizard ah usado ", movichar["movie3"])
+                    if attack<20:
+                        print("el ataque no es efetivo")
+                        hp2-=attack
+                        turnos+=1
+                    elif attack>20:
+                        print("ataque super efectivo")
+                        hp2-=attack
+                        turnos+=1
+                elif att==4:
                     print("charizard ah usado ", movichar["movie3"])
                     if attack<20:
                         print("el ataque no es efetivo")
