b0264b427a9fa9f7371b2ccaa4fc27da48b10fae
deny39-gif <denyvicente15@gmail.com>
2026-06-18 13:02:34 -0400

    poke4

--- pokemon.py
+++ pokemon.py
@@ -1,21 +1,19 @@
 import random, time
 
-attack=random.randint(5,35)
-attlu=random.randint(15,50)
-captu=random.randint(1,4)
 hp1=100
 hp2=500
 
 turnos=1
 
 def captura():
+    captu=random.randint(1,4)
     if captu==1:
         print("el pokemon lugia no capturado")
     elif captu==2:
         print("el pokemon lugia logro escapar de la ball")
     elif captu==3:
         print("el pokemon lugia acaba de romper la ball")
-    elif captu==4:
+    else:
         print("pokemon lugia a sido capturado")
 
 movilugia={
@@ -41,58 +39,102 @@ mochi={
     "ultraball": 64,
 }
 
+def tunolu():
+    global hp1, hp2, turnos
+    desicionlu=random.randint(1,4)
+    if desicionlu==1:
+        print("lugia a usado ", movilugia["movi1"])
+        attlu=random.randint(15,50)
+        hp1-=attlu
+        print("charizard a recibio", attlu,"de daño")
+    elif desicionlu==2:
+        print("lugia usado ", movilugia["movi2"])
+        curacion=random.randint(10,40)
+        hp2+=curacion
+    elif desicionlu==3:
+        print("lugia a usado", movilugia["movi3"])
+        print("lugia a aumentado su defensa")
+    elif desicionlu==4:
+        print("lugia a usado", movilugia["movi4"])
+        attlu=random.randint(15,50)
+        hp1-=attlu
+        print("charizard a recibido", attlu, "de daño")
+
 def pokedex():
     print("Lugia el pokemon legendario de los mares")
     print("tipo: volador y psiquico")
 
 def menu():
-    while True:
-        print("="*30)
-        print("1.- luchar")
-        print("2.- mochila")
-        print("3.- analisas")
-        print("="*30)
-        op=int(input("que opcion elijes?: "))
-        if op==1:
-            print("movimientos de charizard:")
-            
-            print("1.-",movichar["movie1"])
-            print("2.-",movichar["movie2"])
-            print("3.-",movichar["movie3"])
-            print("4.-",movichar["movie4"])
-            att=int(input("que desea hacer??: "))
-            if att==1:
-                print("la defensa de charizard aumentado")
-            elif att==2:
-                print("charizard ah usado ", movichar["movie2"])
-                if attack<20:
-                    print("el ataque no es efetivo")
-                    hp2-=attack
-                    turnos+=1
-                elif attack>20:
-                    print("ataque super efectivo")
-                    hp2-=attack
-                    turnos+=1
-            elif att==3:
-                print("charizard ah usado ", movichar["movie3"])
-                if attack<20:
-                    print("el ataque no es efetivo")
-                    hp2-=attack
-                    turnos+=1
-                elif attack>20:    
-                    print("ataque super efectivo")
-                    hp2-=attack
-                    turnos+=1
-            elif att==4:
-                print("charizard ah usado ", movichar["movie3"])
-                if attack<20:
-                    print("el ataque no es efetivo")
-                    hp2-=attack
-                    turnos+=1
-                elif attack>20:
-                        print("ataque super efectivo")
-                        hp2-=attack
-                        turnos+=1
+    global hp1,hp2,turnos
+    print("="*30)
+    print("1.- luchar")
+    print("2.- mochila")
+    print("3.- analisas")
+    print("="*30)
+    op=int(input("que opcion elijes?: "))
+    if op==1:
+        print("movimientos de charizard:")
+        print("1.-",movichar["movie1"])
+        print("2.-",movichar["movie2"])
+        print("3.-",movichar["movie3"])
+        print("4.-",movichar["movie4"])
+        att=int(input("que desea hacer??: "))
+        if att==1:
+            print("la defensa de charizard aumentado")
+        elif att==2:
+            print("charizard ah usado ", movichar["movie2"])
+            attack=random.randint(5,35)
+            if attack<20:
+                print("el ataque no es efetivo")
+                hp2-=attack
+                turnos+=1
+            else:
+                print("ataque super efectivo")
+                hp2-=attack
+                turnos+=1
+                print("lugia tiene ", hp2," de vida")
+                if hp2>0:
+                    tunolu()
+        elif att==3:
+            print("charizard ah usado ", movichar["movie3"])
+            attack=random.randint(5,35)
+            if attack<20:
+                print("el ataque no es efetivo")
+                hp2-=attack
+                turnos+=1
+            else:    
+                print("ataque super efectivo")
+                hp2-=attack
+                turnos+=1
+                if hp2>0:
+                    tunolu()
+        elif att==4:
+            print("charizard ah usado ", movichar["movie3"])
+            attack=random.randint(5,35)
+            if attack<20:
+                print("el ataque no es efetivo")
+                hp2-=attack
+                turnos+=1
+            else:
+                print("ataque super efectivo")
+                hp2-=attack
+                turnos+=1
+                if hp2>0:
+                    tunolu()
+        elif op==2:
+            print("="*30)
+            print("="*11, "Curar", "="*12)
+            print("="*30)
+            print(".- Pocion:", mochi["pocion"])
+            print(".- Super posion:", mochi["superpocion"])
+            print(".- Restuara total:", mochi["restauratotal"])
+            print("="*30)
+            print("="*10, "Pokeball", "="*10)
+            print("="*30)
+            print(".- Poke Ball:", mochi["pokeball"])
+            print(".- Super Ball:", mochi["superball"])
+            print(".- Ultra Ball:", mochi["ultraball"])
+            opbol=int(input("que objeto desea usar?: "))
 
 
 def inicio():
@@ -101,4 +143,5 @@ def inicio():
     print(f"Charizard: {hp1} de hp")
 
 inicio()
-menu()
+while hp1>0 and hp2>0:
+    menu()
